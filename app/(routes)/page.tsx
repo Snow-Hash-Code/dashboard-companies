161a2322db151aea938785c287b54abe55e32f16
Snow-Hash-Code <gerardoluis206@gmail.com>
2024-10-18 22:09:07 -0600

    2

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,9 @@
 import { CardSummary } from "./components/CardSummary/CardSummary";
 import { BookOpenCheck, UserRound, Waypoints } from "lucide-react";
 import { LastCustomers } from "./components/LastCustomers";
+import { SalesDistributor } from "./components/SalesDistributors";
+import { TotalSubscribers } from "./components/TotalSubscribers";
+import { ListIntegrations } from "./components/ListIntegrations";
 
 export const dataCardSummary = [
   {
@@ -43,9 +46,14 @@ export default function Home() {
         ))}
       </div>
 
-      <div className="grid grid-cols-1 xl:grid-cols-2 md:gap-x-10 mt-12">
+      <div className="grid grid-cols-1 xl:grid-cols-2 md:gap-x-10 mt-12 gap-y-4 md:gap-y-0">
         <LastCustomers />
-        <p>Sales Distributors</p>
+        <SalesDistributor />
+      </div>
+
+      <div className="flex-col xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-center md:gap-x-10">
+        <TotalSubscribers />
+        <ListIntegrations />
       </div>
     </div>
   )
---
+++ lib/formatPrice.ts
@@ -0,0 +1,3 @@
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(price)
+}
